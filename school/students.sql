58b9a40928ea21a51200191b7684cbe92c164837
Schenkel <thi.schenkel@gmail.com>
2021-03-24 11:39:40 +0100

    School finished

--- school/students.sql
+++ school/students.sql
@@ -180,3 +180,55 @@ From students
 WHERE students.city = "Paris"
 
 
+
+-- ## Rapport lvl 3
+-- Faites les requêtes pour :
+
+-- 1. Récupérer les langues et toutes les colonnes de l’étudiant.e d’ID 1
+select students.name, students.city, favorites.class, favorites.sport, languages.name
+From students, favorites, languages, students_languages
+where (students.id = students_languages.student_id)
+    and (students.id = favorites.student_id)
+    and (languages.id = students_languages.language_id)
+    and students.id = 1
+
+-- 2. Récupérer les langues et toutes les colonnes de l’étudiant.e d’ID 4
+select students.name, students.city, favorites.class, favorites.sport, languages.name
+From students, favorites, languages, students_languages
+where (students.id = students_languages.student_id)
+    and (students.id = favorites.student_id)
+    and (languages.id = students_languages.language_id)
+    and students.id = 4
+
+-- 3. Récupérer la colonne langue et le nom de l’étudiant.e d’ID 5
+select students.name, languages.name
+From students, languages, students_languages
+where (students.id = students_languages.student_id)
+    and (languages.id = students_languages.language_id)
+    and students.id = 5
+
+-- 4. Pour chaque étudiant.e.s (6), faîtes une requêtes pour récupérer le nombre de langues parlées par cet étudiant.e.s avec leurs noms et le nombres de langues
+select students.name, count(students_languages.language_id)
+From students, students_languages
+WHERE (students.id = students_languages.student_id)
+group by students.name
+
+
+
+-- ## Bonus
+-- 1. Récupérer les étudiant.e.s qui ont un “e” dans leur prénom
+select students.name
+from students
+where students.name like "%e%"
+
+-- 2. Récupérer le sport préférés des étudiant.e.s qui ont un “e” dans leur prénom
+select students.name, favorites.sport
+from students, favorites
+where (students.id = favorites.student_id)
+    and students.name like "%e%"
+
+-- 3. Récupérer la classe préférées des étudiant.e.s qui ont un “i” dans le nom de leur ville
+select students.name, favorites.class
+from students, favorites
+where (students.id = favorites.student_id)
+    and students.city like "%i%"
